1a76779f46a39b75217e8d4e0c2701cedbb2ad72
Damla Metin Orak <damla91@gmail.com>
2025-06-17 23:16:26 +0300

    app.Use to Create Middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,43 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Middleware #1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #1: Before calling next\r\n");
+
+    await next(context);
+
+    await context.Response.WriteAsync("Middleware #1: After calling next\r\n");
+});
+
+//Middleware #2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
+        
+    await next(context); //Commenting this line makes the Middleware #2 a Terminal Middleware
+
+    await context.Response.WriteAsync("Middleware #2: After calling next\r\n");
+});
+
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
+
+//    await next(context); //Commenting this line will give an arror if you don't use the types of the parameters. You have to call the "next" delegate
+
+//    await context.Response.WriteAsync("Middleware #2: After calling next\r\n");
+//});
+
+//Middleware #3
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Middleware #3: Before calling next\r\n");
+
+    await next(context);
+
+    await context.Response.WriteAsync("Middleware #3: After calling next\r\n");
+});
 
 app.Run();
