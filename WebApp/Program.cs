e4546cffb3a6eddbbf8bf3a894a316e9003a90f7
Damla Metin Orak <damla91@gmail.com>
2025-06-18 11:25:52 +0300

    app.Run to Create Middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -12,13 +12,9 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
 });
 
 //Middleware #2
-app.Use(async (HttpContext context, RequestDelegate next) =>
+app.Run(async (context) =>
 {
-    await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
-        
-    await next(context); //Commenting this line makes the Middleware #2 a Terminal Middleware
-
-    await context.Response.WriteAsync("Middleware #2: After calling next\r\n");
+    await context.Response.WriteAsync("Middleware #2: Processed\r\n");
 });
 
 //app.Use(async (context, next) =>
