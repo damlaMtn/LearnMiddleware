9870c4ecbf6af3a94e3905bcd32787ad298b8279
Damla Metin Orak <damla91@gmail.com>
2025-06-20 14:58:00 +0300

    Custom Exception Handling Middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,8 +3,11 @@ using WebApp.MiddleComponents;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<MyCustomMiddleware>();
+builder.Services.AddTransient<MyCustomExceptionHandler>();
 
 var app = builder.Build();
+
+app.UseMiddleware<MyCustomExceptionHandler>();
 
 
 //Middleware #1
@@ -22,6 +25,8 @@ app.UseMiddleware<MyCustomMiddleware>();
 //Middleware #2
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
+    throw new ApplicationException("Exception for testing.");
+
     await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
 
     await next(context); //Commenting this line makes the Middleware #2 a Terminal Middleware
@@ -40,4 +45,3 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
 });
 
 app.Run();
- 
